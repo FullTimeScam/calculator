3773d46c21b3b623aed15ef040de20f83a0e954f
FullTimeScam <418.gingin@gmail.com>
2024-04-30 13:51:05 +0900

    if

--- main.js
+++ main.js
@@ -3,7 +3,15 @@ const resultElement = document.querySelector(".result");
 
 resultElement.innerHTML = result;
 
+function onClickNumber(number) {
+  if (resultElement.innerHTML.length > 14) return; //길이를 15까지만 제한
+  if (resultElement.innerHTML === "0") {
+    //0일 때는 교체
+    resultElement.innerHTML = number;
+  } else {
+    //0이 아니면 숫자를 계속 추가
+    resultElement.innerHTML += number;
+  }
 
-function onClickNumber(number){
-    resultElement.innerHTML=number;
+  console.log(resultElement.innerHTML.length);
 }
